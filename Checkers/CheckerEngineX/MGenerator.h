36f5364794739e5a874fad42314396a384586dde
robin <checkersprogramming@gmail.com>
2020-07-24 22:23:29 +0200

    final change before our trip

--- Checkers/CheckerEngineX/MGenerator.h
+++ Checkers/CheckerEngineX/MGenerator.h
@@ -93,6 +93,7 @@ void loopCaptures(Position &pos, MoveListe &liste) {
 
 
 inline void getMoves(Position &pos, MoveListe &liste) {
+    liste.reset();
     if (pos.color == BLACK) {
         loopCaptures<BLACK>(pos, liste);
         if (!liste.isEmpty())
@@ -107,6 +108,7 @@ inline void getMoves(Position &pos, MoveListe &liste) {
 }
 
 inline void getCaptures(Position &pos, MoveListe &liste) {
+    liste.reset();
     if (pos.getColor() == BLACK) {
         loopCaptures<BLACK>(pos, liste);
     } else {
