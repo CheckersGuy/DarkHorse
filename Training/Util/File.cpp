2dc241f325a62a8e68b5eeaa669e3931de0605f2
robin <checkersprogramming@gmail.com>
2022-03-30 23:22:58 +0200

    adding some file specific functions

--- Training/Util/File.cpp
+++ Training/Util/File.cpp
@@ -8,36 +8,14 @@
 namespace File {
 
 
-    size_t num_illegal_samples(std::string input){
-        std::ifstream stream(input,std::ios::binary);
-        std::istream_iterator<Sample>begin(stream);
-        std::istream_iterator<Sample>end;
+    bool file_equal(std::string file_one, std::string file_two) {
+        std::ifstream stream_one(file_one, std::ios::binary);
+        std::ifstream stream_two(file_two, std::ios::binary);
 
-        return std::count_if(begin,end,[](Sample s){return !s.position.is_legal();});
-    }
+        std::istreambuf_iterator<char> begin_one(stream_one);
+        std::istreambuf_iterator<char> begin_two(stream_two);
 
-    void remove_duplicates(std::string input, std::string output) {
-        std::ifstream stream(input, std::ios::binary);
-        std::istream_iterator<Sample> begin(stream);
-        std::istream_iterator<Sample> end;
-
-        size_t counter = 0;
-        size_t total_elements = 0;
-        BloomFilter<Sample> filter(5751035027, 10);
-        std::vector<Sample> elements;
-        std::for_each(begin, end, [&](Sample s) {
-            if (!filter.has(s)) {
-                counter++;
-                filter.insert(s);
-                elements.emplace_back(s);
-            }
-            total_elements++;
-        });
-
-        std::ofstream out_stream(output, std::ios::binary);
-        std::copy(elements.begin(), elements.end(), std::ostream_iterator<Sample>(out_stream));
-        std::cout << "Size after removing: " << elements.size() << std::endl;
-        std::cout << "Removed a total of " << total_elements - elements.size() << " elements" << std::endl;
+        return std::equal(begin_one, std::istreambuf_iterator<char>{}, begin_two);
     }
 
 }
--- Training/Util/File.h
+++ Training/Util/File.h
@@ -19,63 +19,10 @@
 #include <filesystem>
 
 namespace File {
-    void remove_duplicates(std::string input, std::string output);
 
-    size_t num_illegal_samples(std::string input);
-
-    template<typename T>
-    void merge_files(std::initializer_list<std::string> files, std::string output) {
-        std::ofstream out_stream(output, std::ios::binary);
-
-        for (auto &file: files) {
-            std::ifstream stream(file, std::ios::binary);
-            std::istream_iterator<T> begin(stream);
-            std::istream_iterator<T> end;
-            std::copy(begin, end, std::ostream_iterator<T>(out_stream));
-        }
-
-    }
+    bool file_equal(std::string file_one,std::string file_two);
 
 
-    template<typename T>
-    void external_shuffle(std::string input) {
-        struct stat stat_buf;
-        int rc = stat(input.c_str(), &stat_buf);
-        auto num_elements = rc / (sizeof(T));
-        auto fd = open(input.c_str(), O_RDWR);
 
-        std::filesystem::path my_path(input);
-        auto size = std::filesystem::file_size(my_path);
-        auto num_examples = size / (sizeof(T));
-
-        T *elements = (Sample *) mmap(NULL, sizeof(Sample) * num_examples, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
-                                      0);
-
-
-        //doing the shiffer yates shuffle
-        std::mt19937_64 generator(11123123ull);
-        auto t1 = std::chrono::duration_cast<std::chrono::milliseconds>
-                (std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-        for (auto i = num_examples - 1; i >= 1; i--) {
-            //getting a random number between 0 and i
-
-            auto t2 = std::chrono::duration_cast<std::chrono::milliseconds>
-                    (std::chrono::high_resolution_clock::now().time_since_epoch()).count();;
-            auto dur = (t2 - t1);
-            if (dur >= 1000) {
-                t1 = t2;
-
-                std::cout << "Counter: " << i<< "\n";
-            }
-
-            std::uniform_int_distribution<size_t> distrib(0, i);
-            auto j = distrib(generator);
-            auto temp = elements[i];
-            elements[i] = elements[j];
-            elements[j] = temp;
-        }
-
-
-    }
 }
 #endif //READING_FILE_H
