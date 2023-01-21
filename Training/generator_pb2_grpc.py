89c11ff16a2f901b18bc11a5aa98fca81c0f4c8e
checkersguy <checkersprogramming@gmail.com>
2023-01-22 00:08:34 +0100

    testing

--- Training/generator_pb2_grpc.py
+++ Training/generator_pb2_grpc.py
@@ -24,6 +24,16 @@ class GeneratorStub(object):
                 request_serializer=generator__pb2.Batch.SerializeToString,
                 response_deserializer=generator__pb2.Response.FromString,
                 )
+        self.get_last_update = channel.unary_unary(
+                '/Generator/get_last_update',
+                request_serializer=generator__pb2.Empty.SerializeToString,
+                response_deserializer=generator__pb2.LastUpdate.FromString,
+                )
+        self.get_new_network = channel.unary_unary(
+                '/Generator/get_new_network',
+                request_serializer=generator__pb2.Empty.SerializeToString,
+                response_deserializer=generator__pb2.Network.FromString,
+                )
 
 
 class GeneratorServicer(object):
@@ -41,6 +51,18 @@ class GeneratorServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def get_last_update(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def get_new_network(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_GeneratorServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -53,6 +75,16 @@ def add_GeneratorServicer_to_server(servicer, server):
                     servicer.upload_batch,
                     request_deserializer=generator__pb2.Batch.FromString,
                     response_serializer=generator__pb2.Response.SerializeToString,
+            ),
+            'get_last_update': grpc.unary_unary_rpc_method_handler(
+                    servicer.get_last_update,
+                    request_deserializer=generator__pb2.Empty.FromString,
+                    response_serializer=generator__pb2.LastUpdate.SerializeToString,
+            ),
+            'get_new_network': grpc.unary_unary_rpc_method_handler(
+                    servicer.get_new_network,
+                    request_deserializer=generator__pb2.Empty.FromString,
+                    response_serializer=generator__pb2.Network.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -95,5 +127,39 @@ class Generator(object):
         return grpc.experimental.unary_unary(request, target, '/Generator/upload_batch',
             generator__pb2.Batch.SerializeToString,
             generator__pb2.Response.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def get_last_update(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Generator/get_last_update',
+            generator__pb2.Empty.SerializeToString,
+            generator__pb2.LastUpdate.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def get_new_network(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Generator/get_new_network',
+            generator__pb2.Empty.SerializeToString,
+            generator__pb2.Network.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
--- Training/server.py
+++ Training/server.py
@@ -6,19 +6,72 @@ from concurrent import futures
 from rich.live import Live
 from rich.table import Table
 import threading
+import os
+import yaml
+def remove_prefix(text,prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+def remove_postfix(text,postfix):
+    if text.endswith(postfix):
+        return text[:len(text)-len(postfix)]
+    return text
+
 
 #to be added
 class Generator(generator_pb2_grpc.GeneratorServicer):
 
-    def __init__(self):
+    def __init__(self,config_file="test.yaml"):
         self.counter =0
         self.thread = None
         self.stop = False
+        self.last_update = None
+        with open(config_file,"r") as f:
+            self.config = yaml.safe_load(f)
+
 
     def __del__(self):
         self.stop = True
         if self.thread is not None:
             self.thread.join()
+
+    def get_window_count(self):
+        directory = os.fsencode("TrainData")
+        max_index = -1000
+        for file in os.listdir(directory):
+            file_name = os.fsdecode(file)
+            if not file_name.startswith(file_name,self.config["name"]+".window"):
+                continue
+            if not file_name.endswith(".train"):
+                continue
+            file_name = remove_prefix(file_name,self.config["name"]+".window")
+            file_name = remove_postfix(file_name,".train")
+            index = int(file_name)
+            if index> max_index:
+                max_index = index
+        return max_index
+
+    def get_latest_network(self):
+        directory = os.fsencode("Networks")
+        net_file = None
+        max_index = -1000
+        for file in os.listdir(directory):
+            file_name = os.fsdecode(file)
+            if not file_name.startswith(self.config["name"]):
+                continue
+            if not file_name.endswith(".quant"):
+                continue
+            file_name = remove_postfix(file_name,".quant")
+            file_name =remove_prefix(file_name,self.config["name"])
+            index = int(file_name)
+            if index>max_index:
+                max_index =index
+                net_file = os.fsdecode(file)
+
+        return net_file
+
+
         
     def upload_batch(self, request, context):
         #print("Got a batch of data")
@@ -26,6 +79,28 @@ class Generator(generator_pb2_grpc.GeneratorServicer):
         self.counter+=len(request.games)
         print(self.counter)
         return generator_pb2.Response(message="Got the batch")
+
+    def get_last_update(self, request, context):
+        return generator_pb2.LastUpdate(timestamp= self.last_update if self.last_update is not None else 0)
+
+    def get_new_network(self, request, context):
+        #here needs to be code to get the latest network
+        #file = open("Networks/bigagain10.quant","rb")
+        #data = file.read()
+        #file.close()
+        file_name =self.get_latest_network()
+        if file_name is None:
+            #do something here
+            return
+
+
+        print("Name:",file_name)
+        file = open("Networks/"+file_name,"rb")
+        data = file.read()
+        file.close()
+        net_msg = generator_pb2.Network()
+        net_msg.data = data
+        return net_msg
 
     def get_table(self):
         table = Table(title="Getting games")
@@ -51,9 +126,15 @@ def server():
     generator.start_ui_thread()
     print("Test")
     generator_pb2_grpc.add_GeneratorServicer_to_server(generator,server)
-    server.add_insecure_port("[::]:50051")
+    server.add_insecure_port("[::]:50052")
     print("Server starting")
     server.start()
     server.wait_for_termination()
 
-server()
+if __name__=="__main__":
+    server()
+
+text="test123.quant"
+text = remove_postfix(text,".quant")
+text = remove_prefix(text,"test")
+print(text)
