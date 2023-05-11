e22b6620f2081dcc374e83dd0e0f4f041845f4f6
checkersguy <checkersprogramming@gmail.com>
2023-05-11 11:48:23 +0200

    testing

--- Checkers/CheckerEngineX/CheckerBoard.h
+++ Checkers/CheckerEngineX/CheckerBoard.h
@@ -1,7 +1,7 @@
- #include <iostream>
+#include "GameLogic.h"
+#include <iostream>
 #include <stdint.h>
-#include <GameLogic.h>
-//Everything related to the checkerboard interface
+// Everything related to the checkerboard interface
 #define CB_WHITE 1
 #define CB_BLACK 2
 #define CB_MAN 4
@@ -30,135 +30,126 @@
 #define CB_RESET_MOVES 1
 #define CB_EXACT_TIME 2
 
-
-struct coor
-{
-int x;
-int y;
+struct coor {
+  int x;
+  int y;
 };
 
-struct CBmove{
-int jumps; 
-int newpiece;
-int oldpiece; 
-struct coor from,to;
-struct coor path[12]; // intermediate squares to jump to
-struct coor del[12]; // squares where men are removed
-int delpiece[12]; // piece type which is removed
+struct CBmove {
+  int jumps;
+  int newpiece;
+  int oldpiece;
+  struct coor from, to;
+  struct coor path[12]; // intermediate squares to jump to
+  struct coor del[12];  // squares where men are removed
+  int delpiece[12];     // piece type which is removed
 };
 
-template<size_t size,typename Generator> constexpr auto get_lut1(Generator&& generator){
-	using Type = decltype(generator(0));
-	std::array<Type,size> result;
+template <size_t size, typename Generator>
+constexpr auto get_lut1(Generator &&generator) {
+  using Type = decltype(generator(0));
+  std::array<Type, size> result;
 
-	for(auto i=0;i<size;++i){
-		result[i]=generator(i);
-	}
-	return result;
+  for (auto i = 0; i < size; ++i) {
+    result[i] = generator(i);
+  }
+  return result;
 };
 
 Position last_position;
-bool engine_initialized=false;
-constexpr std::array<size_t,32>To64 = get_lut1<32>([](size_t index){auto row = index/4; return ((row&1)==0) ? 2*index+1 : 2*index;});
+bool engine_initialized = false;
+constexpr std::array<size_t, 32> To64 = get_lut1<32>([](size_t index) {
+  auto row = index / 4;
+  return ((row & 1) == 0) ? 2 * index + 1 : 2 * index;
+});
 Board game_board;
 
-extern"C" int getmove(
-	int board[8][8],
-	int color,
-	double maxtime,
-	char str[1024],
-	int* playnow,
-	int info,
-	int moreinfo,
-	struct CBmove* move
-);
+extern "C" int getmove(int board[8][8], int color, double maxtime,
+                       char str[1024], int *playnow, int info, int moreinfo,
+                       struct CBmove *move);
 
-//extern"C" unsigned int CB_GETGAMETYPE();	/* return GT_ENGLISH, GT_ITALIAN, ... */
-// INT (WINAPI *CB_ISLEGAL)(Board8x8 board, int color, int from, int to, CBmove *move);
-extern"C" int enginecommand(char command[256], char reply[1024]);
+// extern"C" unsigned int CB_GETGAMETYPE();	/* return GT_ENGLISH,
+// GT_ITALIAN, ... */
+//  INT (WINAPI *CB_ISLEGAL)(Board8x8 board, int color, int from, int to, CBmove
+//  *move);
+extern "C" int enginecommand(char command[256], char reply[1024]);
 
+inline void numbertocoors(int number, int *x, int *y, int gametype) {
+  switch (gametype) {
+  case GT_ITALIAN:
+    number--;                // number e 0...31
+    *y = number / 4;         // *y e 0...7
+    *x = 2 * ((number % 4)); // *x e {0,2,4,6}
+    if (((*y) % 2))          // adjust x on odd rows
+      (*x)++;
+    break;
 
-inline void numbertocoors(int number, int *x, int *y, int gametype)
-{
-	switch (gametype) {
-	case GT_ITALIAN:
-		number--;						// number e 0...31
-		*y = number / 4;				// *y e 0...7
-		*x = 2 * ((number % 4));		// *x e {0,2,4,6}
-		if (((*y) % 2))					// adjust x on odd rows
-			(*x)++;
-		break;
+  case GT_SPANISH:
+    number--;
+    *y = number / 4;
+    *y = 7 - *y;
+    *x = 2 * (3 - (number % 4)); // *x e {0,2,4,6}
+    if (((*y) % 2))              // adjust x on odd rows
+      (*x)++;
+    break;
 
-	case GT_SPANISH:
-		number--;
-		*y = number / 4;
-		*y = 7 - *y;
-		*x = 2 * (3 - (number % 4));	// *x e {0,2,4,6}
-		if (((*y) % 2))					// adjust x on odd rows
-			(*x)++;
-		break;
+  case GT_CZECH: // TODO: check that this is correct!
+    number--;    // number e 0...31
+    number = 33 - number;
+    *y = number / 4;         // *y e 0...7
+    *x = 2 * ((number % 4)); // *x e {0,2,4,6}
+    if (((*y) % 2))          // adjust x on odd rows
+      (*x)++;
+    break;
 
-	case GT_CZECH:						// TODO: check that this is correct!
-		number--;						// number e 0...31
-		number = 33 - number;
-		*y = number / 4;				// *y e 0...7
-		*x = 2 * ((number % 4));		// *x e {0,2,4,6}
-		if (((*y) % 2))					// adjust x on odd rows
-			(*x)++;
-		break;
-
-	default:
-		number--;
-		*y = number / 4;
-		*x = 2 * (3 - number % 4);
-		if ((*y) % 2)
-			(*x)++;
-	}
+  default:
+    number--;
+    *y = number / 4;
+    *x = 2 * (3 - number % 4);
+    if ((*y) % 2)
+      (*x)++;
+  }
 }
 
-inline void numbertocoors(int number, coor *c, int gametype)
-{
-	numbertocoors(number, &c->x, &c->y, gametype);
+inline void numbertocoors(int number, coor *c, int gametype) {
+  numbertocoors(number, &c->x, &c->y, gametype);
 }
 
 /*
  * Give the x,y coordinates for a Board8x8, return the square number (1..32).
  */
-inline int coorstonumber(int x, int y, int gametype)
-{
-	int number;
+inline int coorstonumber(int x, int y, int gametype) {
+  int number;
 
-	switch (gametype) {
-	case GT_ITALIAN:
-		// italian rules
-		number = 1;
-		number += 4 * y;
-		number += x / 2;
-		break;
+  switch (gametype) {
+  case GT_ITALIAN:
+    // italian rules
+    number = 1;
+    number += 4 * y;
+    number += x / 2;
+    break;
 
-	case GT_SPANISH:
-		// spanish rules
-		number = 1;
-		number += 4 * (7 - y);
-		number += (7 - x) / 2;
-		break;
+  case GT_SPANISH:
+    // spanish rules
+    number = 1;
+    number += 4 * (7 - y);
+    number += (7 - x) / 2;
+    break;
 
-	case GT_CZECH:
-		// TODO: make sure this is correct for czech rules
-		number = 1;
-		number += 4 * y;
-		number += x / 2;
-		number = 33 - number;
-		break;
+  case GT_CZECH:
+    // TODO: make sure this is correct for czech rules
+    number = 1;
+    number += 4 * y;
+    number += x / 2;
+    number = 33 - number;
+    break;
 
-	default:
-		number = 0;
-		number += 4 * (y + 1);
-		number -= x / 2;
-	}
+  default:
+    number = 0;
+    number += 4 * (y + 1);
+    number -= x / 2;
+  }
 
-	return number;
+  return number;
 }
 
-
- 
--- Training/Match.h
+++ Training/Match.h
@@ -4,160 +4,153 @@
 
 #ifndef TRAINING_MATCH_H
 #define TRAINING_MATCH_H
-#include <MoveListe.h>
 #include "MGenerator.h"
+#include "fcntl.h"
+#include <MoveListe.h>
+#include <Position.h>
 #include <deque>
-#include <iterator>
+#include <filesystem>
 #include <fstream>
+#include <iterator>
 #include <string>
-#include  <sys/types.h>
+#include <sys/resource.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include "fcntl.h"
-#include <sys/resource.h>
-#include <filesystem>
-#include <Position.h>
 
 inline std::string getPositionString(Position pos) {
-    std::string position;
-    for (uint32_t i = 0; i < 32u; ++i) {
-        uint32_t current = 1u << i;
-        if ((current & (pos.BP & pos.K))) {
-            position += "3";
-        } else if ((current & (pos.WP & pos.K))) {
-            position += "4";
-        } else if ((current & pos.BP)) {
-            position += "1";
-        } else if ((current & pos.WP)) {
-            position += "2";
-        } else {
-            position += "0";
-        }
-    }
-    if (pos.get_color() == BLACK) {
-        position += "B";
+  std::string position;
+  for (uint32_t i = 0; i < 32u; ++i) {
+    uint32_t current = 1u << i;
+    if ((current & (pos.BP & pos.K))) {
+      position += "3";
+    } else if ((current & (pos.WP & pos.K))) {
+      position += "4";
+    } else if ((current & pos.BP)) {
+      position += "1";
+    } else if ((current & pos.WP)) {
+      position += "2";
     } else {
-        position += "W";
+      position += "0";
     }
-    return position;
+  }
+  if (pos.get_color() == BLACK) {
+    position += "B";
+  } else {
+    position += "W";
+  }
+  return position;
 }
 
 struct Engine {
-    enum class State {
-        Idle, Game_Ready, Init_Ready,
-        Update
-    };
-    std::string engine_name;
-    State state;
-    const int &engineRead;
-    const int &engineWrite;
-    bool waiting_response = false;
-    int time_move = 100;
-    int hash_size = 21;
+  enum class State { Idle, Game_Ready, Init_Ready, Update };
+  std::string engine_name;
+  State state;
+  const int &engineRead;
+  const int &engineWrite;
+  bool waiting_response = false;
+  int time_move = 100;
+  int hash_size = 21;
 
-    void initEngine();
+  void initEngine();
 
-    void writeMessage(const std::string &msg);
+  void writeMessage(const std::string &msg);
 
-    void new_move(Move move);
+  void new_move(Move move);
 
-    void newGame(const Position &pos);
+  void newGame(const Position &pos);
 
-    void update();
+  void update();
 
-    Move search();
+  Move search();
 
-    std::string readPipe();
+  std::string readPipe();
 
-    void setHashSize(int hash);
+  void setHashSize(int hash);
 
-    void setTime(int time);
+  void setTime(int time);
 };
-
 
 struct Interface {
 
-    std::array<Engine, 2> engines;
-    Position start_pos;
-    Position pos;
-    bool first_game = true;
-    int first_mover = 0;
-    std::vector<Position> history;
-    size_t pos_counter{0};
+  std::array<Engine, 2> engines;
+  Position start_pos;
+  Position pos;
+  bool first_game = true;
+  int first_mover = 0;
+  std::vector<Position> history;
+  size_t pos_counter{0};
 
-    void process();
+  void process();
 
-    bool is_n_fold(int n);
+  bool is_n_fold(int n);
 
-    bool is_legal_move(Move move);
+  bool is_legal_move(Move move);
 
-    bool is_terminal_state();
+  bool is_terminal_state();
 
-    void reset_engines();
+  void reset_engines();
 
-    void terminate_engines();
+  void terminate_engines();
 };
-
 
 class Match {
 
 private:
-    size_t opening_counter{0};
-    const std::string &first;
-    const std::string &second;
-    std::string arg1,arg2;
-    int time{100};
-	std::pair<int,int> times;
-    int hash_size{21};
-    int maxGames{1000000};
-    int wins_one{0}, wins_two{0}, draws{0};
-    int threads{1};
-    std::vector<std::string> positions;
-    std::string openingBook{"../Training/Positions/drawbook.book"};
+  size_t opening_counter{0};
+  const std::string &first;
+  const std::string &second;
+  std::string arg1, arg2;
+  int time{100};
+  std::pair<int, int> times;
+  int hash_size{21};
+  int maxGames{1000000};
+  int wins_one{0}, wins_two{0}, draws{0};
+  int threads{1};
+  std::vector<std::string> positions;
+  std::string openingBook{"../Training/Positions/train12.book"};
 
 public:
-    explicit Match(const std::string &first, const std::string &second) : first(
-            first), second(second) {
-        std::ifstream stream(openingBook);
+  explicit Match(const std::string &first, const std::string &second)
+      : first(first), second(second) {
+    std::ifstream stream(openingBook);
 
-        if(!stream.good()){
-            std::cerr<<"Could not load the openings"<<std::endl;
-            std::exit(-1);
-        }
-        std::string line;
-        while(std::getline(stream,line)){
-          positions.emplace_back(line);
-        }
-        std::mt19937_64 generator(getSystemTime());
-    };
+    if (!stream.good()) {
+      std::cerr << "Could not load the openings" << std::endl;
+      std::exit(-1);
+    }
+    std::string line;
+    while (std::getline(stream, line)) {
+      positions.emplace_back(line);
+    }
+    std::mt19937_64 generator(getSystemTime());
+  };
 
-    void setMaxGames(int games);
+  void setMaxGames(int games);
 
-    int getMaxGames();
+  int getMaxGames();
 
-    void start();
+  void start();
 
-    void set_arg1(std::string arg);
+  void set_arg1(std::string arg);
 
-    void set_arg2(std::string arg);
+  void set_arg2(std::string arg);
 
-    void setHashSize(int hash);
+  void setHashSize(int hash);
 
-    void setTime(int time);
+  void setTime(int time);
 
-    void setNumThreads(int threads);
+  void setNumThreads(int threads);
 
-    int getNumThreads();
+  int getNumThreads();
 
-    const std::string &getOpeningBook();
+  const std::string &getOpeningBook();
 
-    void setOpeningBook(std::string book);
+  void setOpeningBook(std::string book);
 
-    std::string get_start_pos(size_t& pos_counter);
-	
-	void set_time(int time_one, int time_two);
+  std::string get_start_pos(size_t &pos_counter);
 
+  void set_time(int time_one, int time_two);
 };
 
-
-#endif //TRAINING_MATCH_H
+#endif // TRAINING_MATCH_H
