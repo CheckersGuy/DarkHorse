0685958ff7687b56b826ce2deaf7188121607d3c
checkersguy <checkersprogramming@gmail.com>
2023-05-11 21:25:46 +0200

    small fix to killers

--- Checkers/CheckerEngineX/MoveListe.h
+++ Checkers/CheckerEngineX/MoveListe.h
@@ -1,99 +1,85 @@
 #ifndef CHECKERSTEST_MOVELISTE_H
 #define CHECKERSTEST_MOVELISTE_H
 
-#include "Move.h"
-#include "types.h"
-#include "MovePicker.h"
-#include <array>
 #include "Line.h"
+#include "Move.h"
+#include "MovePicker.h"
 #include "Position.h"
+#include "types.h"
+#include <array>
 
 struct Local {
-    Value alpha, beta;
-    Value best_score{-INFINITE};
-    Depth depth;
-    Ply ply;
-    int i;
-    Move move,previous,previous_own;
-    Move sing_move;
-    Move skip_move;
-    Value sing_score;
+  Value alpha, beta;
+  Value best_score{-INFINITE};
+  Depth depth;
+  Ply ply;
+  int i;
+  Move move, previous, previous_own;
+  Move sing_move;
+  Move skip_move;
+  Value sing_score;
 };
 
 class MoveListe {
 
 private:
-    int moveCounter = 0;
+  int moveCounter = 0;
+
 public:
-    std::array<Move, 40> liste;
+  std::array<Move, 40> liste;
 
-    int length() const;
+  int length() const;
 
-    void add_move(Move next);
+  void add_move(Move next);
 
-    void sort(Position current, Local& local, Move ttMove, int start_index);
+  void sort(Position current, Local &local, Move ttMove, int start_index);
 
-    bool is_empty() const;
+  bool is_empty() const;
 
-    const Move &operator[](int index) const;
+  const Move &operator[](int index) const;
 
-    Move &operator[](int index);
+  Move &operator[](int index);
 
-    bool put_front(Move other);
+  bool put_front(Move other);
 
-	  bool put_front(int start_index, Move other);
+  bool put_front(int start_index, Move other);
 
-    void remove(Move move);
+  void remove(Move move);
 
-    void reset();
+  void reset();
 
+  auto begin() { return liste.begin(); }
 
-    auto begin() {
-        return liste.begin();
-    }
+  auto end() {
+    auto it = liste.begin();
+    std::advance(it, moveCounter);
+    return it;
+  }
 
-    auto end() {
-        auto it = liste.begin();
-        std::advance(it, moveCounter);
-        return it;
-    }
+  MoveListe &operator=(const MoveListe &other);
 
-    MoveListe &operator=(const MoveListe &other);
+  template <MoveType type> inline void visit(uint32_t &maske, uint32_t &next) {
+    add_move(Move{maske, next});
+  };
 
-    template<MoveType type>
-    inline void visit(uint32_t &maske, uint32_t &next) {
-        add_move(Move{maske, next});
-    };
-
-    template<MoveType type>
-    inline void visit(uint32_t &from, uint32_t &to, uint32_t &captures) {
-        add_move(Move{from, to, captures});
-    };
-
-
-
+  template <MoveType type>
+  inline void visit(uint32_t &from, uint32_t &to, uint32_t &captures) {
+    add_move(Move{from, to, captures});
+  };
 };
 
-inline bool MoveListe::is_empty() const {
-    return this->moveCounter == 0;
-}
+inline bool MoveListe::is_empty() const { return this->moveCounter == 0; }
 
 inline const Move &MoveListe::operator[](int index) const {
-    return liste[index];
+  return liste[index];
 }
 
-inline Move &MoveListe::operator[](int index) {
-    return liste[index];
-}
+inline Move &MoveListe::operator[](int index) { return liste[index]; }
 
 inline void MoveListe::add_move(Move next) {
-    liste[this->moveCounter++] = next;
+  liste[this->moveCounter++] = next;
 }
 
-inline int MoveListe::length() const {
-    return moveCounter;
-}
+inline int MoveListe::length() const { return moveCounter; }
 
-
-
-#endif //CHECKERSTEST_MOVELISTE_H
+#endif // CHECKERSTEST_MOVELISTE_H
--- Checkers/CheckerEngineX/MovePicker.h
+++ Checkers/CheckerEngineX/MovePicker.h
@@ -5,39 +5,40 @@
 #ifndef CHECKERENGINEX_MOVEPICKER_H
 #define CHECKERENGINEX_MOVEPICKER_H
 
-#include "types.h"
 #include "Move.h"
-#include "Position.h"
-#include "Network.h"
 #include "MoveListe.h"
-#include "Move.h"
+#include "Network.h"
+#include "Position.h"
+#include "types.h"
 namespace Statistics {
-    class MovePicker {
-    private:
-        std::array<int, 32 * 16> history{0};
-    public:
-		std::array<std::array<Move,MAX_KILLERS>,MAX_PLY> killer_moves;
-	  std::array<std::array<int,32*16>,32*16> counter_history;
-	  std::array<std::array<int,32*16>,32*16> follow_history;
-		Network policy;
-        int get_move_score(Position pos, Move move,Move previous,Move previous_own, Depth depth);
+class MovePicker {
+private:
+  std::array<int, 32 * 16> history{0};
 
-        int get_history_index(Position pos, Move move);
+public:
+  std::array<std::array<Move, MAX_KILLERS>, MAX_PLY> killer_moves;
+  std::array<std::array<int, 32 * 16>, 32 * 16> counter_history;
+  std::array<std::array<int, 32 * 16>, 32 * 16> follow_history;
+  Network policy;
+  int get_move_score(Position pos, Move move, Move previous, Move previous_own,
+                     Depth depth);
 
-        int get_move_score(Position current, Depth depth, int ply, Move move,Move previous,Move previous_own, Move ttMove);
+  int get_history_index(Position pos, Move move);
 
-        void clear_scores();
+  int get_move_score(Position current, Depth depth, int ply, Move move,
+                     Move previous, Move previous_own, Move ttMove);
 
-        void update_scores(Position pos, Move *list, Move move,Move previous,Move previous_own, int depth);
-        
-        static int get_move_encoding(Move move);
-        static int get_policy_encoding(Color color, Move move);
-        void init();
+  void clear_scores();
 
-    };
+  void update_scores(Position pos, Move *list, Move move, Move previous,
+                     Move previous_own, int depth);
 
-    extern MovePicker mPicker;
+  static int get_move_encoding(Move move);
+  static int get_policy_encoding(Color color, Move move);
+  void init();
+};
 
+extern MovePicker mPicker;
 
-}
-#endif //CHECKERENGINEX_MOVEPICKER_H
+} // namespace Statistics
+#endif // CHECKERENGINEX_MOVEPICKER_H
