2a7504b3dd99b80aa829f736bc3cab15ef9d5e0f
checkersguy <checkersprogramming@gmail.com>
2023-05-11 21:25:46 +0200

    small fix to killers

--- Checkers/CheckerEngineX/MoveListe.cpp
+++ Checkers/CheckerEngineX/MoveListe.cpp
@@ -1,82 +1,74 @@
 #include "MoveListe.h"
 #include "MovePicker.h"
 
-void MoveListe::reset() {
-    moveCounter = 0;
-}
+void MoveListe::reset() { moveCounter = 0; }
 
 extern Line mainPV;
 
-void MoveListe::sort(Position current, Local& local, Move ttMove, int start_index) {
+void MoveListe::sort(Position current, Local &local, Move ttMove,
+                     int start_index) {
 
-    if (moveCounter - start_index <= 1)
-        return;
-      std::array<int,40> scores;
+  if (moveCounter - start_index <= 1)
+    return;
+  std::array<int, 40> scores;
 
-    //Statistics::mPicker.policy.compute_incre_forward_pass(current);
-    for (auto i = start_index; i < moveCounter; ++i) {
-        Move m = liste[i];
-        scores[i] =Statistics::mPicker.get_move_score(current, local.depth, local.ply, m,local.previous, local.previous_own, ttMove);
+  // Statistics::mPicker.policy.compute_incre_forward_pass(current);
+  for (auto i = start_index; i < moveCounter; ++i) {
+    Move m = liste[i];
+    scores[i] = Statistics::mPicker.get_move_score(current, local.depth,
+                                                   local.ply, m, local.previous,
+                                                   local.previous_own, ttMove);
+  }
+
+  for (int i = start_index + 1; i < moveCounter; ++i) {
+    const int tmp = scores[i];
+    Move tmpMove = liste[i];
+    int j;
+    for (j = i; j > (start_index) && scores[j - 1] < tmp; --j) {
+      liste[j] = liste[j - 1];
+      scores[j] = scores[j - 1];
     }
-
-    for (int i = start_index + 1; i < moveCounter; ++i) {
-        const int tmp = scores[i];
-        Move tmpMove = liste[i];
-        int j;
-        for (j = i; j > (start_index) && scores[j - 1] < tmp; --j) {
-            liste[j] = liste[j - 1];
-            scores[j] = scores[j - 1];
-        }
-        liste[j] = tmpMove;
-        scores[j] = tmp;
-    }
-
+    liste[j] = tmpMove;
+    scores[j] = tmp;
+  }
 }
 
 void MoveListe::remove(Move move) {
 
-    int i;
-    for (i = 0; i < moveCounter; ++i) {
-        if (liste[i] == move)
-            break;
-    }
+  int i;
+  for (i = 0; i < moveCounter; ++i) {
+    if (liste[i] == move)
+      break;
+  }
 
-    for (int k = i + 1; k < moveCounter; ++k) {
-        liste[k - 1] = liste[k];
-    }
+  for (int k = i + 1; k < moveCounter; ++k) {
+    liste[k - 1] = liste[k];
+  }
 
-    moveCounter = moveCounter - 1;
-
+  moveCounter = moveCounter - 1;
 }
 
-bool MoveListe::put_front(Move other) {
-	return put_front(0,other);
-}
+bool MoveListe::put_front(Move other) { return put_front(0, other); }
 
 bool MoveListe::put_front(int start_index, Move other) {
-    if (moveCounter<= 1)
-			return false;
-
-        auto tmp = liste[start_index];
-        for(auto i=start_index; i<moveCounter; ++i)
-        {
-            if(liste[i]==other) {
-                liste[start_index]=other;
-                liste[i]=tmp;
-                return true;
-            }
-        }
+  if (moveCounter <= 1)
     return false;
 
+  auto tmp = liste[start_index];
+  for (auto i = start_index; i < moveCounter; ++i) {
+    if (liste[i] == other) {
+      liste[start_index] = other;
+      liste[i] = tmp;
+      return true;
+    }
+  }
+  return false;
 }
 
 MoveListe &MoveListe::operator=(const MoveListe &other) {
-    for (auto i = 0; i < other.moveCounter; ++i) {
-        liste[i] = other.liste[i];
-    }
-    this->moveCounter = other.moveCounter;
-    return *this;
+  for (auto i = 0; i < other.moveCounter; ++i) {
+    liste[i] = other.liste[i];
+  }
+  this->moveCounter = other.moveCounter;
+  return *this;
 }
-
-
-
--- Checkers/CheckerEngineX/MovePicker.cpp
+++ Checkers/CheckerEngineX/MovePicker.cpp
@@ -9,30 +9,29 @@ namespace Statistics {
 
 MovePicker mPicker;
 
-
-
 void MovePicker::init() {
- // policy.load_bucket("policy.quant");
- // std::cout<<policy<<std::endl;
+  // policy.load_bucket("policy.quant");
+  // std::cout<<policy<<std::endl;
 }
-
 
 int MovePicker::get_move_encoding(Move move) {
-    int dir;
-    if ((((move.to & MASK_L3) << 3) == move.from) || (((move.to & MASK_L5) << 5) == move.from)) {
-        dir = 0;
-    } else if (((move.to) << 4) == move.from) {
-        dir = 1;
-    } else if (((move.to) >> 4) == move.from) {
-        dir = 2;
-    } else if ((((move.to & MASK_R3) >> 3) == move.from) || (((move.to & MASK_R5) >> 5) == move.from)) {
-        dir = 3;
-    }
-    return 4*move.get_from_index()+dir;
+  int dir;
+  if ((((move.to & MASK_L3) << 3) == move.from) ||
+      (((move.to & MASK_L5) << 5) == move.from)) {
+    dir = 0;
+  } else if (((move.to) << 4) == move.from) {
+    dir = 1;
+  } else if (((move.to) >> 4) == move.from) {
+    dir = 2;
+  } else if ((((move.to & MASK_R3) >> 3) == move.from) ||
+             (((move.to & MASK_R5) >> 5) == move.from)) {
+    dir = 3;
+  }
+  return 4 * move.get_from_index() + dir;
 }
 
-int MovePicker::get_policy_encoding(Color mover,Move move){
-  if(mover == BLACK){
+int MovePicker::get_policy_encoding(Color mover, Move move) {
+  if (mover == BLACK) {
     Move temp;
     temp.from = getMirrored(move.from);
     temp.to = getMirrored(move.to);
@@ -43,139 +42,142 @@ int MovePicker::get_policy_encoding(Color mover,Move move){
 }
 
 int MovePicker::get_history_index(Position pos, Move move) {
-    int t;
+  int t;
 
-    if ((move.from & (pos.BP & pos.K)) != 0) {
-        t = 0;
-    } else if ((move.from & (pos.WP & pos.K)) != 0) {
-        t = 1;
-    } else if ((move.from & pos.BP) != 0) {
-        t = 2;
-    } else if ((move.from & pos.WP) != 0) {
-        t = 3;
-    } else {
-        t = 0;
-    }
+  if ((move.from & (pos.BP & pos.K)) != 0) {
+    t = 0;
+  } else if ((move.from & (pos.WP & pos.K)) != 0) {
+    t = 1;
+  } else if ((move.from & pos.BP) != 0) {
+    t = 2;
+  } else if ((move.from & pos.WP) != 0) {
+    t = 3;
+  } else {
+    t = 0;
+  }
 
-    int orig_sq = move.get_from_index();
-    //direction of the piece
-    int dir = 0;
+  int orig_sq = move.get_from_index();
+  // direction of the piece
+  int dir = 0;
 
-    if ((((MASK_R3 & move.to) >> 3) == move.from) || (((MASK_R5 & move.to) >> 5) == move.from)) {
-        dir = 1;
-    } else if ((((MASK_L3 & move.to) << 3) == move.from) || (((MASK_L5 & move.to) << 5) == move.from)) {
-        dir = 2;
-    } else if ((move.to << 4) == move.from) {
-        dir = 3;
-    }
-    const int index = 16 * orig_sq + 4 * dir + t;
-    return index;
+  if ((((MASK_R3 & move.to) >> 3) == move.from) ||
+      (((MASK_R5 & move.to) >> 5) == move.from)) {
+    dir = 1;
+  } else if ((((MASK_L3 & move.to) << 3) == move.from) ||
+             (((MASK_L5 & move.to) << 5) == move.from)) {
+    dir = 2;
+  } else if ((move.to << 4) == move.from) {
+    dir = 3;
+  }
+  const int index = 16 * orig_sq + 4 * dir + t;
+  return index;
 }
-
-
 
 void MovePicker::clear_scores() {
-    std::fill(history.begin(), history.end(), 0);
-    for(auto i=0; i<killer_moves.size(); ++i) {
-        for(auto k=0; k<MAX_KILLERS; ++k) {
-            killer_moves[i][k]=Move{};
-        }
+  std::fill(history.begin(), history.end(), 0);
+  for (auto i = 0; i < killer_moves.size(); ++i) {
+    for (auto k = 0; k < MAX_KILLERS; ++k) {
+      killer_moves[i][k] = Move{};
     }
-    for(auto i=0; i<counter_history.size(); ++i) {
-        for(auto k=0; k<counter_history[0].size(); ++k) {
-            counter_history[i][k]=0;
-        }
+  }
+  for (auto i = 0; i < counter_history.size(); ++i) {
+    for (auto k = 0; k < counter_history[0].size(); ++k) {
+      counter_history[i][k] = 0;
     }
+  }
 
-  for(auto i=0; i<follow_history.size(); ++i) {
-        for(auto k=0; k<follow_history[0].size(); ++k) {
-            follow_history[i][k]=0;
-        }
+  for (auto i = 0; i < follow_history.size(); ++i) {
+    for (auto k = 0; k < follow_history[0].size(); ++k) {
+      follow_history[i][k] = 0;
     }
-
+  }
 }
-int MovePicker::get_move_score(Position pos, Move move, Move previous,Move previous_own, Depth depth)
-{
-    // const int index = get_move_encoding(pos.get_color(),move);
-    const int index = get_history_index(pos,move);
-    int score = history[index];
+int MovePicker::get_move_score(Position pos, Move move, Move previous,
+                               Move previous_own, Depth depth) {
+  // const int index = get_move_encoding(pos.get_color(),move);
+  const int index = get_history_index(pos, move);
+  int score = history[index];
 
+  if (!previous.is_capture() && !move.is_capture()) {
+    auto counter = counter_history[get_history_index(pos, previous)][index];
+    score += counter;
+  }
+  if (!previous_own.is_empty() && !previous_own.is_capture() &&
+      !move.is_capture()) {
+    auto follow = follow_history[get_history_index(pos, previous_own)][index];
+    score += follow;
+  }
 
-    if(!previous.is_capture() && !move.is_capture()) {
-        auto counter=counter_history[get_history_index(pos,previous)][index];
-       score+=counter;
-    }
-        if(!previous_own.is_empty() && !previous_own.is_capture() && !move.is_capture()) {
-            auto follow= follow_history[get_history_index(pos,previous_own)][index];
-            score+=follow;
-       }
-  
-    return score;
-
+  return score;
 }
 
-int MovePicker::get_move_score(Position current, Depth depth, int ply, Move move,Move previous,Move previous_own, Move ttMove) {
-    if (move == ttMove) {
-        return std::numeric_limits<int32_t>::max();
+int MovePicker::get_move_score(Position current, Depth depth, int ply,
+                               Move move, Move previous, Move previous_own,
+                               Move ttMove) {
+  if (move == ttMove) {
+    return std::numeric_limits<int32_t>::max();
+  }
+
+  if (move.is_capture()) {
+    return (int)Bits::pop_count(move.captures);
+  }
+
+  // auto value  = policy[get_policy_encoding(current.get_color(), move)];
+  // std::cout<<"From: "<<(int)move.get_from_index()<<std::endl;
+  // std::cout<<"To: "<<(int)move.get_to_index()<<std::endl;
+  // std::cout<<value<<std::endl;
+  // return value;
+  /*
+    if (move == killer_moves[ply][1] || move == killer_moves[ply][0]) {
+      return std::numeric_limits<int32_t>::max() - 1000;
     }
-
-    if (move.is_capture()) {
-        return (int) Bits::pop_count(move.captures);
+  */
+  for (auto i = 0; i < MAX_KILLERS; ++i) {
+    if (move == killer_moves[ply][i]) {
+      return std::numeric_limits<int32_t>::max() - 1000;
     }
+  }
 
-    //auto value  = policy[get_policy_encoding(current.get_color(), move)];
-    //std::cout<<"From: "<<(int)move.get_from_index()<<std::endl;
-    //std::cout<<"To: "<<(int)move.get_to_index()<<std::endl;
-    //std::cout<<value<<std::endl;
-    //return value;
-
-    if(move == killer_moves[ply][1] || move == killer_moves[ply][0]) {
-        return std::numeric_limits<int32_t>::max()-1000;
-    }
-    
-
-    return get_move_score(current, move,previous,previous_own, depth);
-
+  return get_move_score(current, move, previous, previous_own, depth);
 }
 
-void update_history_score(int& score, int delta) {
-    score+=delta;
-}
+void update_history_score(int &score, int delta) { score += delta; }
 
+void MovePicker::update_scores(Position pos, Move *liste, Move move,
+                               Move previous, Move previous_own, int depth) {
+  const int index = get_history_index(pos, move);
+  // const int delta = std::min(depth*depth,16*16);;
+  const int delta = depth;
+  update_history_score(history[index], delta);
+  Move top = liste[0];
 
-void MovePicker::update_scores(Position pos, Move *liste, Move move,Move previous,Move previous_own, int depth) {
-    const int index = get_history_index(pos, move);
-    //const int delta = std::min(depth*depth,16*16);;
-    const int delta = depth;
-    update_history_score(history[index],delta);
-    Move top = liste[0];
+  if (!previous.is_capture() && !move.is_capture()) {
+    counter_history[get_history_index(pos, previous)]
+                   [get_history_index(pos, move)] += delta;
+  }
 
-	
-        if(!previous.is_capture() && !move.is_capture()) {
-            counter_history[get_history_index(pos,previous)][get_history_index(pos,move)]+=delta;
-        }
+  if (!previous_own.is_empty() && !previous_own.is_capture() &&
+      !move.is_capture()) {
+    follow_history[get_history_index(pos, previous_own)]
+                  [get_history_index(pos, move)] += delta;
+  }
 
-	
-        if(!previous_own.is_empty() && !previous_own.is_capture() && !move.is_capture()) {
-            follow_history[get_history_index(pos,previous_own)][get_history_index(pos,move)]+=delta;
-        }
+  while (top != move) {
+    top = *liste;
+    int &score = history[get_history_index(pos, top)];
+    update_history_score(score, -delta);
 
-
-    while (top != move) {
-        top = *liste;
-        int& score = history[get_history_index(pos, top)];
-        update_history_score(score,-delta);
-
-
-        if(!previous.is_capture() && !top.is_capture()) {
-            counter_history[get_history_index(pos,previous)][get_history_index(pos,top)]-=delta;
-        }
-       if(!previous_own.is_empty() &&!previous_own.is_capture() && !move.is_capture()) {
-            follow_history[get_history_index(pos,previous_own)][get_history_index(pos,move)]-=delta;
-        }
-
-
-        liste++;
+    if (!previous.is_capture() && !top.is_capture()) {
+      counter_history[get_history_index(pos, previous)]
+                     [get_history_index(pos, top)] -= delta;
     }
+    if (!previous_own.is_empty() && !previous_own.is_capture() &&
+        !move.is_capture()) {
+      follow_history[get_history_index(pos, previous_own)]
+                    [get_history_index(pos, move)] -= delta;
+    }
+
+    liste++;
+  }
 }
-}
+} // namespace Statistics
