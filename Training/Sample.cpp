543bfcefa04bfce5866284bd4d96b1d8ae07da3a
checkersguy <checkersprogramming@gmail.com>
2023-05-14 13:38:39 +0200

    testing

--- Training/Sample.cpp
+++ Training/Sample.cpp
@@ -5,33 +5,33 @@
 #include "Sample.h"
 
 std::string result_to_string(Result result) {
-    if(result == BLACK_WON)
-        return "BLACK_WON";
-    if(result == WHITE_WON)
-        return "WHITE_WON";
-    if(result == DRAW)
-        return "DRAW";
+  if (result == BLACK_WON)
+    return "BLACK_WON";
+  if (result == WHITE_WON)
+    return "WHITE_WON";
+  if (result == DRAW)
+    return "DRAW";
 
-    return "UNKNOWN";
+  return "UNKNOWN";
 }
 bool Sample::operator==(const Sample &other) const {
-    return (position == other.position && result == other.result);
+  return (position == other.position && result == other.result);
 }
 
 bool Sample::operator!=(const Sample &other) const {
-    return !((*this) == other);
+  return !((*this) == other);
 }
 
 std::ofstream &operator<<(std::ofstream &stream, const Sample s) {
-    stream.write((char*)&s,sizeof(Sample));
-    return stream;
+  stream.write((char *)&s, sizeof(Sample));
+  return stream;
 }
 
 std::ifstream &operator>>(std::ifstream &stream, Sample &s) {
-    stream.read((char*)&s,sizeof(Sample));
-    return stream;
+  stream.read((char *)&s, sizeof(Sample));
+  return stream;
 }
 
-bool Sample::is_training_sample() const{
-  return !((position.has_jumps(position.get_color())) ||result == UNKNOWN);
+bool Sample::is_training_sample() const {
+  return !((position.has_jumps(position.get_color())) || result == UNKNOWN);
 }
