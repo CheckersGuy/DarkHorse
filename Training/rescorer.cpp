0037aaba5dac315d1bb016377a8cc58171295c80
checkersguy <checkersprogramming@gmail.com>
2023-07-09 05:40:40 +0200

    noo

--- Training/rescorer.cpp
+++ Training/rescorer.cpp
@@ -8,12 +8,16 @@ int main(int argl, const char **argc) {
   std::string file_name = argc[1];
 
   std::ifstream stream("../Training/TrainData/" + file_name);
+  /*
+    std::istream_iterator<Sample> begin(stream);
+    std::istream_iterator<Sample> end{};
 
-  std::istream_iterator<Sample> begin(stream);
-  std::istream_iterator<Sample> end{};
-
-  std::for_each(begin, end, [](Sample s) {
-    s.position.print_position();
-    std::cout << std::endl;
-  });
+    std::for_each(begin, end, [](Sample s) {
+      s.position.print_position();
+      std::cout << std::endl;
+    });
+    */
+  Sample test;
+  stream >> test;
+  test.position.print_position();
 }
