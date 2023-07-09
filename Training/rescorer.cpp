a869b78b53f719cb74f4587a31146edc9a0ed6a8
checkersguy <checkersprogramming@gmail.com>
2023-07-09 05:45:06 +0200

    still working on it

--- Training/rescorer.cpp
+++ Training/rescorer.cpp
@@ -6,8 +6,13 @@ int main(int argl, const char **argc) {
   std::cout << "Hello I am the rescorer" << std::endl;
 
   std::string file_name = argc[1];
+  std::string path = "../Training/TrainData/" + file_name;
+  std::cout << "Path: " << path << std::endlL;
+  std::ifstream stream(path.c_str());
+  if (!stream.good()) {
+    std::cout << "Could not open the stream" << std::endl;
+  }
 
-  std::ifstream stream("../Training/TrainData/" + file_name);
   /*
     std::istream_iterator<Sample> begin(stream);
     std::istream_iterator<Sample> end{};
