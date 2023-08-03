8b6364fa1d55e2bd3e8e571fc49473b5f6efff8c
checkersguy <checkersprogramming@gmail.com>
2023-08-03 21:28:17 +0200

    testing

--- Training/trainer.py
+++ Training/trainer.py
@@ -27,6 +27,6 @@ if __name__ == "__main__":
 
     trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=epochs, callbacks=[check_point_callback])
 
-    trainer.fit(model, data_loader)
+    trainer.fit(model, data_loader,ckpt_path="Networks/medium=0-v32.ckpt")
 
 
