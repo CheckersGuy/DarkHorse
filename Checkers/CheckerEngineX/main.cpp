9c5cb5e07da0bb259c9ae7c9a817377cdfdceec2
checkersguy <checkersprogramming@gmail.com>
2023-08-09 23:44:22 +0200

    testing

--- Checkers/CheckerEngineX/main.cpp
+++ Checkers/CheckerEngineX/main.cpp
@@ -48,7 +48,7 @@ int main(int argl, const char **argc) {
     net_file = parser.as<std::string>("network");
   } else {
     // net_file = "int8test.quant";
-    net_file = "rescored.quant";
+    net_file = "rescored3.quant";
   }
 
   network.load_bucket(net_file);
