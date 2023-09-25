acd9a1ef31af0346c5688063414f08b0bc8e5e12
checkersguy <checkersprogramming@gmail.com>
2023-09-25 23:25:22 +0200

    almost last commit

--- Checkers/CheckerEngineX/GameLogic.cpp
+++ Checkers/CheckerEngineX/GameLogic.cpp
@@ -203,7 +203,7 @@ Value search(bool in_pv, Board &board, Line &pv, Value alpha, Value beta,
   }
   const Value old_alpha = alpha;
 
-  const Value prob_beta = beta + std::max(10, prob_cut - depth / 3);
+  const Value prob_beta = beta + prob_cut;
   for (auto i = 0; i < liste.length(); ++i) {
     // moveLoop starts here
     Move move = liste[i];
