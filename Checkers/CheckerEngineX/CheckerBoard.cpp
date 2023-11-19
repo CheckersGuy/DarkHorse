7b58f4c44b7943d9ff31392dabedc28d8672e5fb
checkersguy <checkersprogramming@gmail.com>
2023-11-19 04:37:59 +0100

    bla

--- Checkers/CheckerEngineX/CheckerBoard.cpp
+++ Checkers/CheckerEngineX/CheckerBoard.cpp
@@ -62,7 +62,7 @@ extern "C" int getmove(int board[8][8], int color, double maxtime,
   }
 
   if (!engine_initialized) {
-    network.load_bucket("bigbug10.quant");
+    network.load_bucket("bigbug12.quant");
     TT.resize(21);
     Statistics::mPicker.init();
     engine_initialized = true;
