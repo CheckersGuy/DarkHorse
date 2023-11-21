5c7bb03d751f4281b36d0295a927b19172faff67
checkersguy <checkersprogramming@gmail.com>
2023-11-21 23:32:47 +0100

    more

--- Checkers/CheckerEngineX/CheckerBoard.cpp
+++ Checkers/CheckerEngineX/CheckerBoard.cpp
@@ -132,7 +132,7 @@ int enginecommand(char str[256], char reply[1024]) {
 
   if (strcmp(command, "staticevaluation") == 0) {
     if (!engine_initialized) {
-      network.load_bucket("bigbug10.quant");
+      network.load_bucket("bigbug13.quant");
       TT.resize(21);
       Statistics::mPicker.init();
       engine_initialized = true;
