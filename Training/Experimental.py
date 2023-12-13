d04e8c90fb5ec1b38cce7b991cefb82dd56722d9
checkersguy <checkersprogramming@gmail.com>
2023-12-13 19:36:27 +0100

    bla

--- Training/Experimental.py
+++ Training/Experimental.py
@@ -12,7 +12,7 @@ import struct
 import numpy as np
 import string_sum
 from torch.utils.data import DataLoader
-L1 =2*1024
+L1 =2*128
 L2 =32
 L3 = 32
 
@@ -24,7 +24,7 @@ class Network(pl.LightningModule):
         self.val_outputs=[] 
         self.max_weight_hidden = 127.0 / 64.0
         self.min_weight_hidden = -127.0/ 64.0
-        self.gamma = 0.98
+        self.gamma = 0.985
 
 
         self.num_buckets =13
@@ -116,23 +116,29 @@ class Network(pl.LightningModule):
 
     def training_step(self, train_batch, batch_idx):
         self.step()
-        result, move,buckets,psqt_buckets, x = train_batch
+        result,evalu, move,buckets,psqt_buckets, x = train_batch
+        evalu = evalu.to(dtype=torch.float32)
+        evalu=evalu/128.0;
+        evalu = torch.sigmoid(evalu)
         out = self.forward(x,buckets)
-        loss =torch.pow(torch.abs(out-result),2.0).mean()
+        loss =torch.pow(torch.abs(out-evalu),2.0).mean()
         self.log('train_loss', loss.detach(),prog_bar=True)
         return {"loss": loss}
 
 
     def validation_step(self, val_batch, batch_idx):
-        result, move,buckets,psqt_buckets, x = val_batch
+        result,evalu, move,buckets,psqt_buckets, x = val_batch
+        evalu = evalu.to(dtype=torch.float32)
+        evalu=evalu/128.0;
+        evalu=torch.sigmoid(evalu)
         out = self.forward(x,buckets)
-        loss = torch.pow(torch.abs(out - result), 2.0).mean()
+        loss = torch.pow(torch.abs(out - evalu), 2.0).mean()
         self.log('val_loss', loss.detach())
         self.val_outputs.append(loss)
         return {"val_loss": loss.detach()}
 
     def on_validation_epoch_end(self):
-        self.save_quantized_bucket("nopsqt.quant")
+        self.save_quantized_bucket("evalu.quant")
         avg_loss = torch.stack(self.val_outputs).mean()
         self.val_outputs.clear()
         tensorboard_logs = {"avg_val_loss": avg_loss}
@@ -207,206 +213,6 @@ class Network(pl.LightningModule):
         return
 
 
-class Network2(pl.LightningModule):
-
-    def __init__(self):
-        super(Network2, self).__init__()
-        self.layers = []
-      
-        self.max_weight_hidden = 127.0 / 64.0
-        self.min_weight_hidden = -127.0 / 64.0
-        self.gamma = 0.99
-
-
-        self.accu = nn.Linear(120,L1)
-        self.layer_one =nn.Linear(L1//2,L2)
-        self.layer_sec = nn.Linear(L2,L3);
-        self.output = nn.Linear(L3,1)
-        self.layers = [self.accu,self.layer_one,self.layer_sec,self.output]
-        self.val_outputs =[]
-
-    def forward(self, x,buckets):
-        ac_rest,rest= self.accu.forward(x).split(L1-1,dim=1)
-        ac = torch.cat([ac_rest,rest],dim=1)
-        ac = torch.clamp(ac,0.0,1.0)
-        ac_x,ac_y = ac.split(L1//2,dim = 1)
-        ac_out = ac_x.mul(ac_y)*(127.0/128.0)
-
-        l1s = self.layer_one(ac_out)
-        l1c = (127.0/128.0)*torch.clamp(l1s,0.0,1.0)**2
-        
-        l2s = self.layer_sec(l1c)
-        l2c = (127.0/128.0)*torch.clamp(l2s,0.0,1.0)**2
-
-        l3s = self.output(l2c)
-        out = torch.sigmoid(l3s+rest)
-
-        return out
-
-
-
-
-       
-    def step(self):
-        with torch.no_grad():
-            for layer in self.layers[1:]:
-                if isinstance(layer, torch.nn.Linear):
-                    layer.weight.clamp_(self.min_weight_hidden, self.max_weight_hidden)
-
-
-    def configure_optimizers(self):
-        #optimizer = torch.optim.AdamW(self.parameters(),lr=2e-3,weight_decay=1e-3)
-        optimizer = Ranger(self.parameters(),lr=3e-3,betas=(.9, 0.999),weight_decay=3e-4,use_gc=False,gc_loc=False)
-        #optimizer = RangerAdaBelief(self.parameters(),lr=1e-3)
-        #optimizer = adabelief_pytorch.AdaBelief(self.parameters(),lr=1e-3,betas=(0.9,0.999))
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=self.gamma)
-        #scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,steps_per_epoch=60000,epochs=120,max_lr=3e-3,cycle_momentum=False)
-        return [optimizer],[scheduler]
-
-
-    def training_step(self, train_batch, batch_idx):
-        self.step()
-        result, move,buckets,psqt_buckets, x = train_batch
-        out = self.forward(x,buckets)
-        loss =torch.pow(torch.abs(out-result),2.6).mean()
-        tensorboard_logs = {"avg_val_loss": loss}
-        self.log('train_loss', loss,prog_bar=True)
-        return {"loss": loss, "log": tensorboard_logs}
-
-    def validation_step(self, val_batch, batch_idx):
-        result, move,buckets,psqt_buckets, x = val_batch
-        out = self.forward(x,buckets)
-        loss = torch.pow(torch.abs(out - result), 2.6).mean()
-        self.log('val_loss', loss.detach())
-        self.val_outputs.append(loss)
-        return {"val_loss": loss.detach()}
-
-    def on_validation_epoch_end(self):
-        self.save_quantizedtest2("bigbug8.quant")
-        torch.save(self.state_dict(),"data6.pt")
-        avg_loss = torch.stack(self.val_outputs).mean()
-        self.val_outputs.clear()
-        tensorboard_logs = {"avg_val_loss": avg_loss}
-        self.log('loss', avg_loss, prog_bar=True)
-        print(avg_loss)
-        return {"loss": avg_loss, "log": tensorboard_logs}
-
-
-    def save_quantizedtest2(self, output):
-        self.step()
-        min16 = np.iinfo(np.int16).min
-        max16 = np.iinfo(np.int16).max
-
-        min8 = np.iinfo(np.int8).min
-        max8 = np.iinfo(np.int8).max
-
-        device = torch.device("cpu")
-        device_gpu = torch.device("cuda")
-        self.to(device)
-        ft_weights_buf = bytearray()
-        ft_bias_buf = bytearray()
-        hidden_buf = bytearray()
-
-        num_weights = 0
-        num_bias = 0
-        file = open(output, "wb")
-
-
-        layer = self.layers[0]
-        weights = layer.weight.detach().numpy().flatten("F")
-        weights = weights * 127.0
-        weights = np.round(weights)
-        weights = np.clip(weights, min16, max16)
-        weights = weights.astype(np.int16)
-        bias = layer.bias.detach().numpy().flatten("F")
-        bias = bias * 127.0
-        bias = np.round(bias)
-        bias = np.clip(bias, min16, max16)
-        bias = bias.astype(np.int16)
-        ft_weights_buf.extend(weights.tobytes())
-        ft_bias_buf.extend(bias.tobytes())
-        num_weights += len(weights)
-        num_bias += len(bias)
-
-
-        for layer in self.layers[1:]:
-            weights = layer.weight
-            weights = layer.weight.detach().numpy().flatten()
-            weights = weights*64.0
-            weights = np.round(weights)
-            weights = np.clip(weights,min8,max8)
-            weights = weights.astype(np.int8)
-            bias = layer.bias.detach().numpy().flatten()
-            bias = bias*(127.0 * 64.0)
-            bias = np.round(bias)
-            bias = bias.astype(np.int32)
-            hidden_buf.extend(weights.tobytes())
-            hidden_buf.extend(bias.tobytes())
-        
-        file.write(ft_weights_buf)
-        file.write(ft_bias_buf)
-        
-        file.write(hidden_buf)
-
-        file.close()
-        self.to(device_gpu)
-
-
-
-class ConvNet(pl.LightningModule):
-
-    def __init__(self):
-        super(Network2, self).__init__()
-        self.layers = []
-      
-        self.max_weight_hidden = 127.0 / 64.0
-        self.min_weight_hidden = -127.0 / 64.0
-        self.gamma = 0.99
-
-        self.val_outputs =[]
-
-    def forward(self, x,buckets):
-       pass
-
-
-
-
-       
-
-    def configure_optimizers(self):
-        #optimizer = torch.optim.AdamW(self.parameters(),lr=1e-3,weight_decay=0)
-        optimizer = Ranger(self.parameters(),lr=5e-3,betas=(.9, 0.999),weight_decay=0,use_gc=False,gc_loc=False)
-        #optimizer = RangerAdaBelief(self.parameters(),lr=1e-3)
-        #optimizer = adabelief_pytorch.AdaBelief(self.parameters(),lr=1e-3,betas=(0.9,0.999))
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=self.gamma)
-        #scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,steps_per_epoch=60000,epochs=120,max_lr=3e-3,cycle_momentum=False)
-        return [optimizer],[scheduler]
-
-
-    def training_step(self, train_batch, batch_idx):
-        result, move,buckets, x = train_batch
-        out = self.forward(x,buckets)
-        loss =torch.pow(torch.abs(out-result),2.6).mean()
-        tensorboard_logs = {"avg_val_loss": loss}
-        self.log('train_loss', loss,prog_bar=True)
-        return {"loss": loss, "log": tensorboard_logs}
-
-    def validation_step(self, val_batch, batch_idx):
-        result, move,buckets, x = val_batch
-        out = self.forward(x,buckets)
-        loss = torch.pow(torch.abs(out - result), 2.6).mean()
-        self.log('val_loss', loss.detach())
-        self.val_outputs.append(loss)
-        return {"val_loss": loss.detach()}
-
-    def on_validation_epoch_end(self):
-        torch.save(self.state_dict(),"data6.pt")
-        avg_loss = torch.stack(self.val_outputs).mean()
-        self.val_outputs.clear()
-        tensorboard_logs = {"avg_val_loss": avg_loss}
-        self.log('loss', avg_loss, prog_bar=True)
-        print(avg_loss)
-        return {"loss": avg_loss, "log": tensorboard_logs}
 
 
 
@@ -431,14 +237,14 @@ class BatchDataSet(torch.utils.data.IterableDataset):
     def __next__(self):
         input_size = 120
         results = np.zeros(self.batch_size, dtype=np.float32)
+        evals =  np.zeros(self.batch_size, dtype=np.int16)
         moves = np.zeros(self.batch_size, dtype=np.int64)
         buckets = np.zeros(self.batch_size, dtype=np.int64)
         psqt_buckets = np.zeros(self.batch_size, dtype=np.int64)
         inputs = np.zeros(self.batch_size*input_size, dtype=np.float32)
-        self.loader.testing(inputs,results,buckets,psqt_buckets)
-        psqt_buckets = np.zeros(self.batch_size, dtype=np.int64)
+        self.loader.testing(inputs,results,evals,buckets,psqt_buckets)
 
-        return results.reshape(self.batch_size,1), moves.reshape(self.batch_size,1),buckets.reshape(self.batch_size,1), psqt_buckets.reshape(self.batch_size,1),inputs.reshape(self.batch_size,input_size)
+        return results.reshape(self.batch_size,1),evals.reshape(self.batch_size,1), moves.reshape(self.batch_size,1),buckets.reshape(self.batch_size,1), psqt_buckets.reshape(self.batch_size,1),inputs.reshape(self.batch_size,input_size)
 
 
 
