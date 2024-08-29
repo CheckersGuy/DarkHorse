d52b4ae5269bcc8a6072274117f415283ed17518
checkersguy <checkersprogramming@gmail.com>
2024-08-29 21:56:01 +0200

    and again working on that particular part of my code

--- Training/Rust/src/main.rs
+++ Training/Rust/src/main.rs
@@ -36,16 +36,19 @@ pub fn perft(pos: Position, depth: i32) -> usize {
 }
 
 fn main() -> anyhow::Result<()> {
-    /* let mut reader = BufReader::new(File::open("/mnt/e/adjudication2rescored.samples")?);
-        for sample in reader.iter_samples().take(1000) {
-            if let Sample::SampleType::Fen(ref position) = sample.position {
-                let pos = Pos::Position::try_from(position.as_str())?;
-                pos.print_position();
-                println!("Result: {:?}", sample.result);
-                println!();
-            }
+    let mut reader = BufReader::new(File::open("/mnt/e/newformat.samples")?);
+
+    for game in reader.iter_games().take(1000) {
+        println!("NewGame");
+        println!("---------------------------");
+        for sample in game.iter().rev() {
+            sample.position.print_position();
+            println!("Result: {:?}", sample.result);
+            println!();
         }
-    */
+        println!("---------------------------");
+    }
+
     /*
         Data::remove_samples(
             "/mnt/e/newtry11rescoredmlhshuffledx.samples",
@@ -66,20 +69,18 @@ fn main() -> anyhow::Result<()> {
         "../TrainData/merged2.samples",
     )?;
     */
-    /*let mut generator = Generator::new(
+    /* let mut generator = Generator::new(
             String::from("../Positions/ultrabook2.pos"),
-            String::from("/mnt/e/adjudication3.samples"),
+            String::from("/mnt/e/newformat.samples"),
             14,
             100000,
         );
 
-
-
         generator.time = 1;
 
-        //generator.prev_file = Some("/mnt/e/finalrescored/paritysuperiorityshuffled.samples");
-        generator.generate_games()?;
+    generator.generate_games()?;
     */
+    //generator.prev_file = Some("/mnt/e/finalrescored/paritysuperiorityshuffled.samples");
 
     /*Data::create_subset(
             "/mnt/e/policyultimateshuffled.samples",
@@ -91,7 +92,7 @@ fn main() -> anyhow::Result<()> {
     //Data::create_book("../Positions/drawbook.book", "testbook.pos", 6)?;
 
     //let fen_string = "B:W30,29:B4,24";
-    let base = Base::new("E:\\kr_english_wld", 2000, 10).unwrap();
+    /*let base = Base::new("E:\\kr_english_wld", 2000, 10).unwrap();
 
     let test = Position::try_from("B:WK2,29:BK3,K12").expect("Could not parse fen");
     test.print_position();
@@ -118,6 +119,7 @@ fn main() -> anyhow::Result<()> {
     for square in test_position.iter() {
         println!("{:?}", square);
     }
+    */
 
     /*
         let result = base.probe("W:W8,6,K13:BK4,7,5,11,9").unwrap();
