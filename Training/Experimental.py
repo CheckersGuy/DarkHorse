f2318264082e0a58b9dd3aba8577181e2380ad1b
checkersguy <checkersprogramming@gmail.com>
2025-06-15 16:59:37 +0200

    test

--- Training/Experimental.py
+++ Training/Experimental.py
@@ -14,7 +14,7 @@ import numpy as np
 import string_sum
 from torch.utils.data import DataLoader
 from splus import SPlus
-L1 =2*8192
+L1 =2*4096
 L2 =32
 L3 = 32
 
@@ -30,7 +30,7 @@ class Network(pl.LightningModule):
         self.val_outputs=[] 
         self.max_weight_hidden = 127.0 / 64.0
         self.min_weight_hidden = -127.0/ 64.0
-        self.gamma = 0.93
+        self.gamma = 0.985
         self.run_name = run_name
 
 
--- Training/trainer.py
+++ Training/trainer.py
@@ -12,8 +12,8 @@ import torch
 if __name__ == "__main__":
     batch_size = 8192 
     epochs = 605
-    model = Experimental.Network(run_name="biggo")
-    data_loader = Experimental.LitDataModule(train_data="/mnt/e/evalcombinednext.samples",
+    model = Experimental.Network(run_name="hope")
+    data_loader = Experimental.LitDataModule(train_data="/mnt/e/evalcloudbig.rescored.shuffled.samples",
     val_data="/mnt/e/validation.samples",
     batch_size=batch_size, buffer_size=300000000)
     
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=epochs, callbacks=[check_point_callback],limit_val_batches=0)
 
-    trainer.fit(model, data_loader);
+    trainer.fit(model, data_loader,ckpt_path="Networks/hope.ckpt");
 
 
 
